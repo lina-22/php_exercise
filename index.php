2dad35373fb001bec7d6948c9778a279ee7cdd73
Fahinur Haque <lina-22@SuperMac.local>
2021-10-25 05:50:40 +0200

    php index work

--- index.php
+++ index.php
@@ -10,4 +10,56 @@ echo "<br>";
 echo "<br>";
 echo $myPrenom;
 
+$myAge = 31;
+echo "<br>";
+echo "my age is  " .$myAge; 
+
+
+
+
+
+
+echo "<br>";
+
+
+echo "how r you ? hope you are fine. and the days going on well";
+echo "<br>";
+
+$mySchoolName= BTRI;
+
+
+
+echo "my school name was " .$mySchoolName  . " high school";
+echo "<br>";
+echo "<br>";
+
+echo "le type de la phrase is " .gettype($mySchoolName);
+echo "<br>";
+echo "<br>";
+
+
+
+
+// calcul on php
+
+$nb1= 10;
+$nb2= 5;
+
+ echo $nb1+$nb2;
+ echo "<br>";
+
+
+ $calcul1= 4+8;
+ $calcul2= 8-3;
+ $calcul3= 4*8;
+ $calcul4= 9/2;
+ 
+ echo $calcul1;
+ echo "<br>";
+ echo $calcul2;
+ echo "<br>";
+ echo $calcul3;
+ echo "<br>";
+ echo $calcul4;
+
 ?>
