fa1e1291bf248ff139d781bc7be8fbca3ed6ea95
Fahinur Haque <lina-22@SuperMac.local>
2021-10-27 20:58:32 +0200

    my workX

---
+++ calcul.php
@@ -0,0 +1,39 @@
+<?php
+
+$nb1= 9;
+$nb2= 7;
+
+echo "$nb1+$nb2";
+
+echo "$nb1+$nb2";
+
+echo "my name is lina, lina is good girl";
+
+$calculA= 3+9;
+echo "<br>";
+echo $calculA;
+
+$calculB= 7-1;
+echo "<br>";
+echo $calculB;
+
+$calculC= 9*2;
+echo "<br>";
+echo $calculC;
+
+
+$calculD= 8/3;
+echo "<br>";
+echo $calculD;
+
+
+// today works
+
+$nb1=9;
+$nb2=3;
+
+echo "nb1 = ".$nb1." et nb2 = ".$nb2. "<br>";
+
+
+
+?>
--- index.php
+++ index.php
@@ -62,4 +62,64 @@ $nb2= 5;
  echo "<br>";
  echo $calcul4;
 
+
+ echo "<br>";
+
+
+// today works
+echo "<br>";
+
+
+ $nb1= 9;
+ $nb2= 7;
+ 
+ echo "$nb1+$nb2";
+ echo "<br>";
+ 
+ echo "my name is lina, lina is good girl.";
+ 
+ $calculA= 3+9;
+ echo "<br>";
+ echo $calculA;
+ 
+ $calculB= 7-1;
+ echo "<br>";
+ echo $calculB;
+ 
+ $calculC= 9*2;
+ echo "<br>";
+ echo $calculC;
+ echo "<br>";
+ 
+ 
+ $calculD= 8%3;
+ echo "<br>";
+ echo "<br>";
+ echo $calculD;
+ echo "<br>";
+
+
+$nb1=9;
+$nb2=3;
+
+echo "nb1 = ".$nb1." et nb2 = ".$nb2. "<br>";
+
+echo "resultat de 8%3 =" .$calculD. "<br>";
+
+$calculD++;
+echo $calculD. "<br>" ;
+// echo "<br>";
+
+$calculD--;
+echo $calculD
+?>
+
+<hr>
+
+<?php
+$monNom = Kaisan;
+$monPrenom = Sayes;
+
+echo $monNom;
+
 ?>
